7bfd0a27918d2d6c2f5dfa8a34dda7135d5ad1b1
Jaey Kim <jkim232@ncsu.edu>
2025-10-29 04:48:58 -0400

    [Implementation] Created the problem system.

--- v1.py
+++ v1.py
@@ -7,9 +7,10 @@ def getMagnitude(p1, p2):
 
 
 class PointMassNode:
-    def __init__(self, mass, position=None):
+    def __init__(self, mass, position=None, velocity=None):
         self.mass = mass            # Data stored in the node
         self.position = position      # Tuple or coordinate (optional)
+        self.velocity = velocity
         self.prev = None              # Reference to previous node
         self.next = None              # Reference to next node
 
@@ -17,36 +18,40 @@ class PointMassNode:
         return f"PointMassNode(mass={self.mass}, position={self.position})"
 
 class FixedSupportNode:
-    def __init__(self, mass, position=None):
+    def __init__(self, mass, position=None, velocity=None):
         self.mass = mass            # Data stored in the node
         self.position = position      # Tuple or coordinate (optional)
+        self.velocity = None
         self.prev = None              # Reference to previous node
         self.next = None              # Reference to next node
 
     def __repr__(self):
         return f"FixedSupportNode(mass={self.mass}, position={self.position})"
 
-class PointMassLinkedList:
+class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
     
-    def add(self, mass, position=None):
+    def add(self, mass, position=None, velocity=None):
         if mass != 0:
-            new_node = PointMassNode(mass, position)
+            new_node = PointMassNode(mass, position, velocity)
         else:
-            new_node = FixedSupportNode(mass, position)
+            new_node = FixedSupportNode(mass, position, velocity)
         
         # case: first node
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+            self.size += 1
             return new_node
         
         # otherwise attach to tail
         new_node.prev = self.tail
         self.tail.next = new_node
         self.tail = new_node
+        self.size += 1
         return new_node
 
     def get(self, index):
@@ -60,17 +65,25 @@ class PointMassLinkedList:
         raise IndexError("Index out of range")
 
 
-lst = PointMassLinkedList()
+class PointMassSystem:
+    def __init__(self, list):
+        self.list = list
+        self.q = 3
+
+list = LinkedList()
 
 step = 1 / 15
 
-lst.add(mass=0.0, position=[0,0])
+list.add(mass=0.0, position=[0,0], velocity=[0,0])
 for i in range(14):
     x = (i+1) * step
-    lst.add(mass=0.05, position=[x,0])
-lst.add(mass=0.0, position=[1,0])
+    list.add(mass=0.05, position=[x,0], velocity=[0,0])
+list.add(mass=0.0, position=[1,0], velocity=[0,0])
 
-current = lst.head
+p1 = PointMassSystem(list)
+
+current = p1.list.head
 while current is not None:
     print(current)
     current = current.next
+print(list.size)
